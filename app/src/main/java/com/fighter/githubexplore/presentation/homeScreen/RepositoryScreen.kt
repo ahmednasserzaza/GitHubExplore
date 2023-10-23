3a3caa2788a26ce6a62b468f23f9436b2919d25e
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-10-23 18:53:37 +0300

    create state hoisting

--- app/src/main/java/com/fighter/githubexplore/presentation/homeScreen/RepositoryScreen.kt
+++ app/src/main/java/com/fighter/githubexplore/presentation/homeScreen/RepositoryScreen.kt
@@ -1,13 +1,16 @@
 package com.fighter.githubexplore.presentation.homeScreen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 
 @Composable
-fun RepositoryScreen() {
-    Repository()
+fun RepositoryScreen(viewModel: RepositoryViewModel) {
+    val state by viewModel.state.collectAsState()
+    Repository(state)
 }
 
 @Composable
-fun Repository() {
+fun Repository(state: RepositoryUiState) {
 
 }
